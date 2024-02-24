e421fce64f135141ccddbdcf5bbc24dce6118147
michael <michaeliraphay@gmail.com>
2024-02-24 15:27:02 +0300

    My frist commit

---
+++ New folder/ABOUT.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ONLINE SAFARIS & TOURS AFRICA</title>
+    <link rel="stylesheet" href="css.main/web.css">
+</head> 
+<body>
+    <header class="header">
+        <div class="main">
+            <div class="logo">
+                <a href="HOME1.php"><img src="main.image/logo.png"></a>
+            </div>
+<nav class="navbar">
+	<label class="comname">ONLINE SAFARIS & TOURS AFRICA</label> 
+    <a href="HOME1.php">home</a>
+    <a href="ABOUT.php">about us</a>
+    <a href="login.php">Sign in</a>
+</nav>
+ </div>
+    </header>
+
+    <nav class="hometext">
+        <h1>about us</h1>
+		<div class="name">
+			ONLINE SAFARIS & TOURS AFRICA
+		</div>
+    </nav>
+
+<div class="hdlogo">
+	<img src="main.image/logo.png">
+</div>
+
+<div class="about1">
+<div class="aboutmain">
+    <h2>Our story in ONLINE SAFARIS & TOURS AFRICA</h2> <br>
+    <p>Our inspiration is to push the boundaries of experiential engagement while showcasing the finest of cultural experiences & personal immersions.</p>
+</div>
+
+<div class="lastp">
+    <h5>At ONLINE SAFARIS & TOURS AFRICA, “listen and learn” are the keywords and cornerstone of our approach to any project – by listening, we learn to appreciate and understand the goals of an event or a journey. Our first goal is to acquire a complete understanding of the client’s objectives.</5>
+    <p>Also at ONLINE SAFARIS & TOURS AFRICA, our first and foremost endeavour is to put you at the centre of our universe. Only when we completely understand your idea of travel, we start weaving your personalised travel story. Our tour designers are there for you at every step – from the time you envision your travel dream till you are back home safely with your unique souvenirs and story. </p>
+</div>
+
+<div class="aboutpc">
+    <img src="main.image/story.online.jpg">
+</div>
+</div>
+
+<div class="about2">
+<div class="aboutmainb">
+    <h2>Why Choose Us for Interior Work:</h2>
+    <p>Core Competency: Ground handling is our greatest strength.</p>
+</div>
+
+<div class="lastpb">
+    <h5>We open doors that are inaccessible to most. Attention to detail and local product knowledge are the foundations of Travmic Incentives.</h5>
+    <p>We have remunerative and economical travel plans, luxury tailor-made vacation programs and best in the industry contacts that gives us an edge over our competitors.</p>
+</div>
+
+<div class="aboutpcb2">
+    <img src="main.image/choose us.jpg">
+</div>
+
+<div class="aboutpcb">
+	<img src="main.image/cruiser.jpg">
+</div>
+
+</div>
+
+
+
+
+	<!-- Team Section -->
+	<section class="team-section style-two">
+		<div class="auto-container">
+			<!-- Sec Title -->
+			<div class="sec-title light centered">
+				<h2>Our Team</h2>
+				<div class="text">The success of ONLINE SAFARIS & TOURS AFRICA is the result of a winning combination of differentiated products, deliverd with personalized service consistenly by our passionate local teams.</div>
+			</div> <br> <br> <br>
+			
+			<div class="clearfix">
+				
+				<!-- Team Block -->
+				<div class="team-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box">
+						<div class="image">
+							<img src="team/team-1.jpg" width="350px" height="350px">
+							<div class="overlay-box">
+								<ul class="social-icons">
+									<h1>contacts:</h1><br>
+									<li><a href="#"><i class="fab fa-skype"><img src="icons/Facebook.png" height="25.5px" width="25.5px"></i></a></li>
+									<li><a href="#"><i class="fab fa-facebook"> <img src="icons/WhatsApp.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/instagram1.png" height="24.5px" width="24.5px"></i></a></li>
+									<li><a href="#"><i class="fab fa-twitter-square"><img src="icons/Email-blue-icon.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Contact.png" height="25px" width="25px"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="lower-content">
+							<h3><a href="about.html">Violeth Msuya</a></h3>
+							<div class="designation">Chief Executive Officer</div>
+						</div>
+					</div>
+				</div>
+				
+				<!-- Team Block -->
+				<div class="team-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box ">
+						<div class="image">
+							<img src="team/team-2.jpg" width="350px" height="350px">
+							<div class="overlay-box">
+								<ul class="social-icons">
+									<h1>contacts:</h1><br>
+									<li><a href="#"><i class="fab fa-skype"><img src="icons/Facebook.png" height="25.5px" width="25.5px"></i></a></li>
+									<li><a href="#"><i class="fab fa-facebook"> <img src="icons/WhatsApp.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/instagram1.png" height="24.5px" width="24.5px"></i></a></li>
+									<li><a href="#"><i class="fab fa-twitter-square"><img src="icons/Email-blue-icon.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Contact.png" height="25px" width="25px"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="lower-content">
+							<h3><a href="about.html">Cathbert Irafay</a></h3>
+							<div class="designation">Safari Advisor</div>
+						</div>
+					</div>
+				</div>
+				
+				<!-- Team Block -->
+				<div class="team-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box">
+						<div class="image">
+							<img src="team/team-3.jpg" width="350px" height="350px">
+							<div class="overlay-box">
+								<ul class="social-icons">
+									<h1>contacts:</h1><br>
+									<li><a href="#"><i class="fab fa-skype"><img src="icons/Facebook.png" height="25.5px" width="25.5px"></i></a></li>
+									<li><a href="#"><i class="fab fa-facebook"> <img src="icons/WhatsApp.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/instagram1.png" height="24.5px" width="24.5px"></i></a></li>
+									<li><a href="#"><i class="fab fa-twitter-square"><img src="icons/Email-blue-icon.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Contact.png" height="25px" width="25px"></i></a></li>
+							</div>
+						</div>
+						<div class="lower-content">
+							<h3><a href="about.html">Esther Raphael</a></h3>
+							<div class="designation">Safari agent</div>
+						</div>
+					</div>
+				</div>
+				
+				<!-- Team Block -->
+				<div class="team-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box">
+						<div class="image">
+							<img src="team/team-4.jpg" width="350px" height="350px">
+							<div class="overlay-box">
+								<ul class="social-icons">
+									<h1>contacts:</h1><br>
+									<li><a href="#"><i class="fab fa-skype"><img src="icons/Facebook.png" height="25.5px" width="25.5px"></i></a></li>
+									<li><a href="#"><i class="fab fa-facebook"> <img src="icons/WhatsApp.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/instagram1.png" height="24.5px" width="24.5px"></i></a></li>
+									<li><a href="#"><i class="fab fa-twitter-square"><img src="icons/Email-blue-icon.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Contact.png" height="25px" width="25px"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="lower-content">
+							<h3><a href="about.html">Sylvester Gody</a></h3>
+							<div class="designation">Safari driver</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+<div class="mission">
+	<img src="main.image/logo.png" width="500px">
+	<div class="inner-column">
+		<center>
+			<h2>Our Company Mission</h2> <br>
+	</center>
+		&nbsp; To demonstrate our creativity, innovation and commitment in a &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; manner which sets us apart from others in the business. <br> <br>
+   		&nbsp; Our mission is to become preferred partner for our overseas tour &nbsp; &nbsp; &nbsp; &nbsp; operators & clients’ and to help them drive change and growth by &nbsp; &nbsp; &nbsp; &nbsp; carefully listening to their requirements and offering customized and &nbsp; &nbsp; tailor-made solutions.
+		<br> <br>
+	<center>
+		<h2>Oline Safaris & Tours Africa help you for classic safari…</h2>
+	</center>
+					&nbsp; To improve the welfare of Tanzanians through tourism. <br>
+					&nbsp; To target and carefully identify the markets which we want to &nbsp; &nbsp;&nbsp;develop.
+				
+		</div>
+	</div>
---
+++ New folder/LOGO.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ONLINE SAFARIS & TOURS AFRICA</title>
+    <link rel="stylesheet" href="css.main/web.css">
+</head> 
+<body>
+    <header class="header">
+        <div class="main">
+            <div class="logo">
+                <a href="HOME.php"><img src="main.image/logo.png"></a>
+            </div>
+<nav class="navbar">
+	<label class="comname">ONLINE SAFARIS & TOURS AFRICA</label> 
+</nav>
+ </div>
+    </header>
+
+    <nav class="hometext">
+        <h1>about us</h1>
+		<div class="name">
+			ONLINE SAFARIS & TOURS AFRICA
+		</div>
+    </nav>
+
+<div class="hdlogo">
+	<img src="main.image/logo.png">
+</div>
+
+<div class="about1">
+<div class="aboutmain">
+    <h2>Our story in ONLINE SAFARIS & TOURS AFRICA</h2> <br>
+    <p>Our inspiration is to push the boundaries of experiential engagement while showcasing the finest of cultural experiences & personal immersions.</p>
+</div>
+
+<div class="lastp">
+    <h5>At ONLINE SAFARIS & TOURS AFRICA, “listen and learn” are the keywords and cornerstone of our approach to any project – by listening, we learn to appreciate and understand the goals of an event or a journey. Our first goal is to acquire a complete understanding of the client’s objectives.</5>
+    <p>Also at ONLINE SAFARIS & TOURS AFRICA, our first and foremost endeavour is to put you at the centre of our universe. Only when we completely understand your idea of travel, we start weaving your personalised travel story. Our tour designers are there for you at every step – from the time you envision your travel dream till you are back home safely with your unique souvenirs and story. </p>
+</div>
+
+<div class="aboutpc">
+    <img src="main.image/story.online.jpg">
+</div>
+</div>
+
+<div class="about2">
+<div class="aboutmainb">
+    <h2>Why Choose Us for Interior Work</h2>
+    <p>Core Competency: Ground handling is our greatest strength.</p>
+</div>
+
+<div class="lastpb">
+    <h5>We open doors that are inaccessible to most. Attention to detail and local product knowledge are the foundations of Travmic Incentives.</h5>
+    <p>We have remunerative and economical travel plans, luxury tailor-made vacation programs and best in the industry contacts that gives us an edge over our competitors.</p>
+</div>
+
+<div class="aboutpcb2">
+    <img src="main.image/choose us.jpg">
+</div>
+
+<div class="aboutpcb">
+	<img src="main.image/cruiser.jpg">
+</div>
+
+</div>
+
+
+
+
+	<!-- Team Section -->
+	<section class="team-section style-two">
+		<div class="auto-container">
+			<!-- Sec Title -->
+			<div class="sec-title light centered">
+				<h2>Our Team</h2>
+				<div class="text">The success of ONLINE SAFARIS & TOURS AFRICA is the result of a winning combination of differentiated products, deliverd with personalized service consistenly by our passionate local teams.</div>
+			</div> <br> <br> <br>
+			
+			<div class="clearfix">
+				
+				<!-- Team Block -->
+				<div class="team-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box">
+						<div class="image">
+							<img src="team/team-1.jpg" width="350px" height="350px">
+							<div class="overlay-box">
+								<ul class="social-icons">
+									<h1>contact:</h1><br>
+									<li><a href="#"><i class="fab fa-skype"><img src="icons/Facebook.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-facebook"> <img src="icons/WhatsApp.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Instagram.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-twitter-square"><img src="icons/Email-blue-icon.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Contact.png" height="25px" width="25px"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="lower-content">
+							<h3><a href="about.html">Violeth Msuya</a></h3>
+							<div class="designation">Chief Executive Officer</div>
+						</div>
+					</div>
+				</div>
+				
+				<!-- Team Block -->
+				<div class="team-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box ">
+						<div class="image">
+							<img src="team/team-2.jpg" width="350px" height="350px">
+							<div class="overlay-box">
+								<ul class="social-icons">
+									<h1>contact:</h1><br>
+									<li><a href="#"><i class="fab fa-skype"><img src="icons/Facebook.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-facebook"> <img src="icons/WhatsApp.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Instagram.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-twitter-square"><img src="icons/Email-blue-icon.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Contact.png" height="25px" width="25px"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="lower-content">
+							<h3><a href="about.html">Cathbert Irafay</a></h3>
+							<div class="designation">Safari Advisor</div>
+						</div>
+					</div>
+				</div>
+				
+				<!-- Team Block -->
+				<div class="team-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box">
+						<div class="image">
+							<img src="team/team-3.jpg" width="350px" height="350px">
+							<div class="overlay-box">
+								<ul class="social-icons">
+									<h1>contact:</h1><br>
+									<li><a href="#"><i class="fab fa-skype"><img src="icons/Facebook.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-facebook"> <img src="icons/WhatsApp.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Instagram.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-twitter-square"><img src="icons/Email-blue-icon.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Contact.png" height="25px" width="25px"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="lower-content">
+							<h3><a href="about.html">Esther Raphael</a></h3>
+							<div class="designation">Safari agent</div>
+						</div>
+					</div>
+				</div>
+				
+				<!-- Team Block -->
+				<div class="team-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box">
+						<div class="image">
+							<img src="team/team-4.jpg" width="350px" height="350px">
+							<div class="overlay-box">
+								<ul class="social-icons">
+									<h1>contact:</h1><br>
+									<li><a href="#"><i class="fab fa-skype"><img src="icons/Facebook.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-facebook"> <img src="icons/WhatsApp.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Instagram.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-twitter-square"><img src="icons/Email-blue-icon.png" height="25px" width="25px"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin"><img src="icons/Contact.png" height="25px" width="25px"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="lower-content">
+							<h3><a href="about.html">Sylvester Gody</a></h3>
+							<div class="designation">Safari driver</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+<div class="mission">
+	<img src="main.image/logo.png" width="500px">
+	<div class="inner-column">
+		<center>
+		<h2>Our Company Mission</h2>
+		To demonstrate our creativity, innovation and commitment in a manner which sets us apart from others in the business.
+   		Our mission is to become preferred partner for our overseas tour operators & clients’ and to help them drive change and growth by carefully listening to their requirements and offering customized and tailor-made solutions.
+		<br> <br>
+</center>
+<center>
+		<h3>ONLINE SAFARIS & TOURS AFRICA help you for classic safari…</h3>
+	</center>
+					<li>To improve the welfare of Tanzanians through tourism.</li>
+					<li>To target and carefully identify the markets which we want to develop.</li>
+				</ul>
+		</div>
+	</div>
+
+
+
+</body> 
+</html>
